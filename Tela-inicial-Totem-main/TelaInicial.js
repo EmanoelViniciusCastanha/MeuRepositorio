79c11f5bfd58cf01020117e83566772ba20e3e46
ManuzinhoXD <111919840+EmanoelViniciusCastanha@users.noreply.github.com>
2022-08-24 01:02:20 -0300

    Tela Inicial  75% pronta
    Falta os botoes, e sobrepor o logo

---
+++ Tela-inicial-Totem-main/TelaInicial.js
@@ -0,0 +1,45 @@
+// window.addEventListener("load", (e) => {
+    // const icon = [
+        // 'fa-solid fa-door-closed',
+        // 'fa-solid fa-location-arrow',
+        // 'fa-solid fa-globe',
+        // 'fa-solid fa-mobile-screen',
+        // "fa-solid fa-people-group",
+        // "fa-solid fa-car",
+        // "fa-solid fa-truck",
+        // "fa-solid fa-thumbs-up",
+        // "fa-regular fa-envelope",
+        // "fa-solid fa-display",
+        // "fa-solid fa-people-group",
+        // "fa-solid fa-car",
+        // "fa-regular fa-envelope",
+        // "fa-solid fa-face-grin-wide",
+        // "fa-solid fa-heart",
+        // "fa-solid fa-mobile-screen",
+        // "fa-solid fa-truck",
+        // "fa-solid fa-thumbs-up",
+
+// ];
+//     // const icon = document.getElementById("fundoAnimado").innerHTML;
+    
+//     let iconWidth = Math.floor(window.innerWidth / 80) - 1;
+//     let iconHeight = Math.floor(window.innerHeight / 80) ;
+//     let htmlFinal = '';
+
+
+//     for (let j=0; j<iconHeight; j++){
+//         htmlFinal += '<div class="row">';
+//         let iconAnterior;
+//         let iconRandom;
+//         for (let i=0; i<iconWidth; i++){
+//             do{
+//                 iconRandom = Math.floor(Math.random()*icon.length)
+
+//             }while(iconAnterior==iconRandom);
+//             iconAnterior=iconRandom
+//             htmlFinal += "<i class='" + icon[iconRandom] + "'></i>";
+//         }
+//         htmlFinal += '</div>';
+//     }
+//     document.getElementById("fundoAnimado").innerHTML = htmlFinal;
+// });
